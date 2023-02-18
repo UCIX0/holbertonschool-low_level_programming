519a896be71faf67c4748c458389c6c0eed0b6f3
UCIX210 <javier.uc@outlook.com>
2023-02-17 18:13:53 -0600

    holiii

---
+++ 101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function main
+ * Return: 1 always
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
