13ca542b54f27fa580517b70d1b73e4f2c9bfc6c
UCIX210 <javier.uc@outlook.com>
2023-03-02 18:29:14 -0600

    100 prueba

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert string to integer
+ * @s: string to convert
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i;
+	int multiplier = 1;
+	int negatigncount = 0;
+	int unit = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			negatigncount++;
+		}
+		if (s[i] <= '9' && s[i] >= '0')
+		{
+			unit = 1;
+			multiplier++;
+		}
+	}
+	return (1);
+}
