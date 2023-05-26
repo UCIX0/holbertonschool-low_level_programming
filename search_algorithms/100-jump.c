c1b23b6da5956a7cba563f0d5008ac6149647e69
UCIX0 <javier.uc@outlook.com>
2023-05-26 14:47:02 -0600

    searchadve3

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -2,28 +2,28 @@
 #include <math.h>
 
 /**
- * busqueda_salto - busca un valor en un array de
+ * jump_search - busca un valor en un array de
  * enteros utilizando el algoritmo de Búsqueda por Saltos
  *
  * @array: array de entrada
- * @size: tamaño del array
+ * @size: tamano del array
  * @value: valor a buscar
  * Return: índice del número
  */
-int busqueda_salto(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	size_t tamaño = size;
+	size_t tamano = size;
 	int valor = value;
 	int indice, m, k, previo;
 
-	if (array == NULL || tamaño == 0)
+	if (array == NULL || tamano == 0)
 		return (-1);
 
-	m = (int)sqrt((double)tamaño);
+	m = (int)sqrt((double)tamano);
 	k = 0;
 	previo = indice = 0;
 
-	while (indice < (int)tamaño && array[indice] < valor)
+	while (indice < (int)tamano && array[indice] < valor)
 	{
 		printf("Valor revisado array[%d] = [%d]\n", indice, array[indice]);
 
@@ -36,7 +36,7 @@ int busqueda_salto(int *array, size_t size, int value)
 
 	printf("Valor encontrado entre índices [%d] y [%d]\n", previo, indice);
 
-	for (; previo <= indice && previo < (int)tamaño; previo++)
+	for (; previo <= indice && previo < (int)tamano; previo++)
 	{
 		printf("Valor revisado array[%d] = [%d]\n", previo, array[previo]);
 		if (array[previo] == valor)
