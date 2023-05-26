669b574346833659d9b32115df199706b93408d1
UCIX0 <javier.uc@outlook.com>
2023-05-26 13:53:00 -0600

    search9

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
-void print_array(int *array, int start, int end);
-int binary_search_recursive(int *array, int start, int end, int value);
+
 int binary_search(int *array, size_t size, int value);
 #endif /* _SEARCH_ALGOS_H_ */
