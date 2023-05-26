b433b836cc8da17252f40b161f1c63c77ae0ed16
UCIX0 <javier.uc@outlook.com>
2023-05-26 13:45:35 -0600

    search8

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,39 +1,46 @@
-#include <stdio.h>
+#include "search_algos.h"
 
-void print_array(int *array, size_t start, size_t end)
+void print_array(int *array, int start, int end)
 {
-	size_t i;
-
 	printf("Searching in array: ");
-	for (i = start; i < end; i++)
+	for (int i = start; i <= end; i++)
 	{
 		printf("%d", array[i]);
-		if (i < end - 1)
+		if (i < end)
 			printf(", ");
 	}
 	printf("\n");
 }
 
+int binary_search_recursive(int *array, int start, int end, int value)
+{
+	if (start > end)
+	{
+		return -1; 
+	}
+
+	int mid = start + (end - start) / 2;
+
+	print_array(array, start, end);
+
+	if (array[mid] == value)
+	{
+		return mid; 
+	}
+	else if (array[mid] > value)
+	{
+		return binary_search_recursive(array, start, mid - 1, value); 
+	}
+	else
+	{
+		return binary_search_recursive(array, mid + 1, end, value); 
+	}
+}
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start = 0;
-	size_t mid;
-	size_t end = size;
-
 	if (!array)
 		return (-1);
 
-	while (start < end)
-	{
-		print_array(array, start, end);
-		mid = start + (end - start) / 2;
-		if (array[mid] < value)
-			start = mid + 1;
-		else if (array[mid] > value)
-			end = mid;
-		else
-			return mid;
-	}
-
-	return (-1);
+	return binary_search_recursive(array, 0, size - 1, value);
 }
