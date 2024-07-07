68db40da9de6de0e0c8bce337273bef7aa77e3d9
Javier Uc Ix <javier.uc@outlook.com>
2024-07-06 20:27:10 -0600

    need loops 1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * jack_bauer - ultomo digito absoluto
  *
@@ -6,13 +7,7 @@
  */
 void jack_bauer(void)
 {
-	int hours, minutes;
+	int h;
+	int m;
 
-	for (hours = 0; hours < 24; hours++)
-	{
-		for (minutes = 0; minutes < 60; minutes++)
-		{
-			printf("%02d:%02d\n", hours, minutes);
-		}
-	}
 }
