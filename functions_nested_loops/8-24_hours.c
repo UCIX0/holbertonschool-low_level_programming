6570ac19c26af9cde1a9a3c00b6e227c693fb874
Javier Uc Ix <javier.uc@outlook.com>
2024-07-06 20:36:12 -0600

    need loops 1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * jack_bauer - ultomo digito absoluto
@@ -13,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			putchar('0' + (h / 10));
-			putchar('0' + (h % 10));
-			putchar(':');
-			putchar('0' + (m / 10));
-			putchar('0' + (m % 10));
-			putchar('\n');
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
 		}
 	}
 }
