61dd1e8e53b0f98603d23a8c4002769370ed6c60
Javier Uc Ix <javier.uc@outlook.com>
2024-07-06 20:36:53 -0600

    need loops 1

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,22 +12,22 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		putchar('0');
+		_putchar('0');
 		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
-			putchar(',');
-			putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 			if (c <= 9)
 			{
-				putchar(' ');
+				_putchar(' ');
 		}
 			else
 			{
-				putchar((c / 10) + '0');
+				_putchar((c / 10) + '0');
 			}
-			putchar((c % 10) + '0');
+			_putchar((c % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
