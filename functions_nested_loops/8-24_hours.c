5cd79a40978cd9485c8b28dcbce979daa675b9cd
Javier Uc Ix <javier.uc@outlook.com>
2024-07-06 20:35:33 -0600

    need loops 1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,5 +7,18 @@
  */
 void jack_bauer(void)
 {
-	_putchar('\n');
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			putchar('0' + (h / 10));
+			putchar('0' + (h % 10));
+			putchar(':');
+			putchar('0' + (m / 10));
+			putchar('0' + (m % 10));
+			putchar('\n');
+		}
+	}
 }
