efafdf7b4e0daa15ab9b58fe59ea3f393a6d0f13
Javier Uc Ix <javier.uc@outlook.com>
2024-07-13 23:52:49 -0600

    pointer arrays

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Cambia la direccion de mememoria 98
+ * @n: integer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i, o;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	for (o = 0; o < i; o += 2)
+	{
+		_putchar(str[o]);
+	}
+	_putchar('\n');
+}
