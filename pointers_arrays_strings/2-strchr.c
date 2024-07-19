34dd626c575eef8caaf9176ab7067b8f45553768
Javier Uc Ix <javier.uc@outlook.com>
2024-07-19 03:57:44 -0600

    pointer array real33

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,28 @@
 #include "main.h"
-/**
- * _strchr - s
- *
- * @s: s
- * @c: s
- *
- * Return: s
- * else return NULL
- */
 
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to find.
+ *
+ * Return: A pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found.
+ */
 char *_strchr(char *s, char c)
 {
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
 }
