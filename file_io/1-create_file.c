5988979e117a67d506aa2ab1fdee0f8e46597e97
Javier Uc Ix <javier.uc@outlook.com>
2024-08-31 00:04:27 -0600

    io1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,39 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - appends text at the end of a file.
- * @filename: the name of the file to read
- * @text_content: cadena terminada en NULL para agregar al final del archivo
- * Return: 1 on success and -1 on failure
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file, write_bytes_file;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	if (text_content == NULL)
+
+	if (text_content != NULL)
 	{
-		text_content = "";
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	/**
-	* file = creat(filename, S_IRUSR + S_IWUSR);
-	* es lo mismo que
-	* file = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR + S_IWUSR);
-	*/
-	file = creat(filename, S_IRUSR + S_IWUSR);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, text_content, len);
+	if (w == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 
-	write_bytes_file = write(file, text_content, strlen(text_content));
-	if (write_bytes_file == -1)
-	{
-		return (-1);
-	}
-	close(file);
+	close(fd);
+
 	return (1);
 }
